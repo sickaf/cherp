dae1efbd47fdebe4052b126509bb005193aa4580
Cody <cody.ko@gmail.com>
2014-10-26 23:54:23 +1100

    all views now work properly with ejs

--- routes/routes.js
+++ routes/routes.js
@@ -3,13 +3,12 @@ module.exports = function(app, passport) {
 	// =====================================
 	// HOME PAGE (with login links) ========
 	// =====================================
-	app.get('/', isLoggedIn, function(req, res) {
-		 res.sendfile('public/index.html');
+	app.get('/', function(req, res) {
+		 res.render('index');
 	});
 
 	app.get('/login', function(req, res) {
 		res.render('login', { message: req.flash('loginMessage') });
-		// res.sendfile('public/login.html');
 	});
 
 	// process the login form
@@ -25,8 +24,7 @@ module.exports = function(app, passport) {
 	// show the signup form
 	app.get('/signup', function(req, res) {
 		// render the page and pass in any flash data if it exists
-		res.sendfile('public/signup.html');
-		// res.render('signup.ejs', { message: req.flash('signupMessage') });
+		res.render('signup', { message: req.flash('signupMessage') });
 	});
 
 	// process the signup form
@@ -54,12 +52,11 @@ module.exports = function(app, passport) {
 	// =====================================
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
-	// app.get('/profile', isLoggedIn, function(req, res) {
-	// 	res.sendfile(rootFilePath+'/public/profile.html');
-	// 	// res.render('profile.ejs', {
-	// 	// 	user : req.user // get the user out of session and pass to template
-	// 	// });
-	// });
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
 
 	// =====================================
 	// LOGOUT ==============================
@@ -68,48 +65,15 @@ module.exports = function(app, passport) {
 		req.logout();
 		res.redirect('/login');
 	});
-
-	/* GET New User page. */
-	app.get('/newmessage', function(req, res) {
-	    res.sendfile('public/newmessage.html');
-	});
-
-	/* POST to Add User Service */
-	app.post('/addmessagetodatabase', function(req, res) {
-
-		//set our internal DB variable
-		var db = req.db;
-
-		var userName = req.body.username;
-		var userMessage = req.body.usermessage;
-
-		//set our collection
-		var collection = db.get('messagecollection');
-
-		//submit to the DB
-		collection.insert({
-			"username" : userName,
-			"message" : userMessage
-		}, function (err, doc) {
-			if(err) {
-				res.send("lol there was a problem adding data to the DB");
-			}
-			else {
-				//res.location("/");
-				//res.redirect("/");
-				res.send("okay maybe that worked");
-			}
-		});
-	});
 }
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
 	// if user is authenticated in the session, carry on 
-	// if (req.user) {
+	if (req.user) {
 		return next();
-	// }
+	}
 
 	// if they aren't redirect them to the login
 	res.redirect('/login');
